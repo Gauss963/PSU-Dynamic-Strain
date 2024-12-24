9906a54f30065acd5b1077dce4e09d866bb0585a
Gauss963 <gauss963@gmail.com>
2024-12-24 16:04:05 +0800

    first commit

---
+++ Code/DataProcessor.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+def voltage_to_strain(raw_voltage: float) -> float:
+    '''
+    Convert raw voltage readings from a strain gauge circuit to strain.
+
+    The function assumes a Wheatstone bridge circuit with a strain gauge 
+    and uses the following relationship:
+    
+    strain = (raw_voltage / Vex / Gain) * (2 / GF)
+    
+    Args:
+        raw_voltage (float): The measured voltage output from the Wheatstone bridge circuit (in volts).
+        
+    Constants:
+        Vex  (float): Excitation voltage applied to the Wheatstone bridge (in volts). Default is 4.98 V.
+        GF   (float): Gauge factor of the strain gauge (dimensionless). Default is 2.12.
+        Rg   (float): Resistance of the strain gauge (in ohms). Default is 350 Ohm.
+        Gain (float): Amplification factor of the signal (dimensionless). Default is 1000.
+    
+    Returns:
+        float: Calculated strain (dimensionless).
+    
+    Example:
+        >>> voltage_to_strain(0.002)
+        1.9178082191780822e-06
+    '''
+    Vex = 4.98    # Excitation voltage in volts
+    GF = 2.12     # Gauge factor
+    Rg = 350      # Resistance of strain gauge in ohms (not directly used)
+    Gain = 1000   # Amplification factor
+    
+    strain = raw_voltage / Vex / Gain * 2 / GF
+    
+    return strain
+
+
+def shear_strain_to_stress(E: float, poisson_ratio: float, strain: float) -> float:
+    '''
+    Calculate stress from shear strain by converting Young's modulus (E) to shear modulus (G).
+    
+    Args:
+        E (float): Young's modulus (elastic modulus) in units of stress (e.g., Pa).
+        strain (float): Shear strain (dimensionless).
+        poisson_ratio (float): Poisson's ratio (dimensionless).
+        
+    Returns:
+        float: Shear stress in the same units as Young's modulus.
+    '''
+    
+    G = E / (2 * (1 + poisson_ratio))
+    stress = G * strain
+    
+    return stress
---
+++ Code/FolderActions.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+filename = '../Data/p5972e1.npz'
+
+
+# data = np.load('/Users/hueyke/Downloads/p5993e.npz', allow_pickle=True)
+data = np.load(filename, allow_pickle = True)
+data = data["experiment"][()]
+
+# run 1 is the second run named shs1
+# event 2 is the thrid event
+event = data['runs'][1]['events'][2]
+
+
+# this is the raw strain in voltage
+# gage factor is 2
+# I forgot the excitation voltage... Let me get back to you. It's definitely <= 15V.
+# you need to convert the raw voltage strain data in Voltage to strain.
+# gage 6 is the 7th strain gage
+# gages 0--12 are exy
+# gages 13--15 are eyy of gage 0, 6, and 12
+# using gage 6 gives you both exy and eyy to fit simultaneously 
+# locations of each gage can be found at event['strain']['locations']
+exy6 = event['strain']['original']['raw'][6]
+eyy6 = event['strain']['original']['raw'][12]
+plt.plot(event['strain']['original']['time'], exy6 - exy6[0], label=r'$\epsilon_{xy}$')
+plt.plot(event['strain']['original']['time'], eyy6 - eyy6[0], label=r'$\epsilon_{yy}$')
+plt.savefig('../Plot/p5972_test1.pdf', dpi = 300)
+plt.show()
+
+
+# info about the strain gage
+# KFGS-5-350-D17-16 Strain Gage Package of 10
+# Series: KFGS
+# Gage length: 5 mm
+# Gage resistance: 350 ohm
+# Gage pattern: Triaxial
+# Applicable Linear Expansion Coefficient (x10–6/°C ): 16
+# Lead wire: 25mm long silver coated copper wire
+# 10 pcs / package
