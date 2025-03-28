7ba872cec8f3a554732eee78c1d62058ac25258f
Gauss963 <gauss963@gmail.com>
2025-03-28 10:20:56 +0800

    Concluding FFT and iFFT.

--- Code/DataProcessor.py
+++ Code/DataProcessor.py
@@ -63,6 +63,28 @@ def highpass_filter(data, cutoff, fs, order=4):
     filtered_data = scipy.signal.filtfilt(b, a, data)
     return filtered_data
 
+def apply_taper(signal:np.ndarray, taper_ratio: float = 0.05) -> np.ndarray:
+    """
+    Apply a Tukey taper to a signal.
+
+    Parameters:
+        signal (numpy array): The input signal.
+        taper_ratio (float): The proportion of the signal to taper (default: 5%).
+
+    Returns:
+        numpy array: The tapered signal.
+    """
+    N = len(signal)
+    taper = np.hanning(int(2 * taper_ratio * N))  # Create a Hann window for the taper
+    window = np.ones(N)
+    
+    # Apply the taper at the beginning
+    window[: len(taper) // 2] = taper[: len(taper) // 2]
+    # Apply the taper at the end
+    window[-len(taper) // 2 :] = taper[-len(taper) // 2 :]
+    
+    return signal * window
+
 def fitting_function(X_c: float, C_f: float, Gamma: float, x: float|np.ndarray, y:float) -> float:
     
     # Gamma = 0.21  # Fracture energy (J/m^2)
@@ -82,3 +104,61 @@ def chi_square(X_c: float, Gamma: float, C_f: float, X: np.ndarray, Y: np.ndarra
     # chi2 = sum( (data_i - model_i)^2 / sigma_i^2 )
     chi2 = np.sum((Y - MODEL)**2)
     return chi2
+
+def do_deconvolution(SIGNAL_1: np.ndarray, 
+                     SIGNAL_2: np.ndarray, 
+                     water_level: float =  0.05, 
+                     damp_ratio: float = 0.05,
+                     stabilizing_type: str = 'W') -> np.ndarray:
+    
+    NUMERATOR = np.fft.fft(SIGNAL_1)
+    DENUMERATOR = np.fft.fft(SIGNAL_2)
+
+
+    max_denominator = np.max(np.abs(DENUMERATOR))
+    water_level_value = water_level * max_denominator
+
+
+    # Do water leveling
+    STABILIZED_DENOMINATOR = DENUMERATOR
+    for i in range(len(DENUMERATOR)):
+        if np.abs(DENUMERATOR[i]) < water_level_value:
+            STABILIZED_DENOMINATOR[i] = ( DENUMERATOR[i] / np.abs(DENUMERATOR[i]) ) * water_level_value
+    DECONV_STABILIZED_WTER = NUMERATOR / STABILIZED_DENOMINATOR
+    DECONV_TIME_DOMAIN_WTER = np.fft.ifft(DECONV_STABILIZED_WTER)
+
+
+    # Do damped
+    damp_value = np.average(np.abs(DENUMERATOR)**2) * damp_ratio
+    DECONV_STABILIZED_DAMP = (NUMERATOR * np.conjugate(DENUMERATOR)) / (DENUMERATOR * np.conjugate(DENUMERATOR) + damp_value)
+    DECONV_TIME_DOMAIN_DAMP = np.fft.ifft(DECONV_STABILIZED_DAMP)
+
+
+    # Choose type
+    if stabilizing_type == 'W':
+        DECONV_TIME_DOMAIN = DECONV_TIME_DOMAIN_WTER
+    if stabilizing_type == 'D':
+        DECONV_TIME_DOMAIN = DECONV_TIME_DOMAIN_DAMP
+
+
+    
+    # Do shifting
+    total_len = int(len(DECONV_TIME_DOMAIN))
+    midpoint = int(len(DECONV_TIME_DOMAIN) / 2)
+    DECONV_TIME_DOMAIN_SHIFTED = np.append(DECONV_TIME_DOMAIN[midpoint: total_len-1], DECONV_TIME_DOMAIN[0:midpoint-1])
+
+    return np.real(DECONV_TIME_DOMAIN_SHIFTED)
+
+
+def get_ccf_full(DATA_FLOOR_1: np.ndarray, DATA_FLOOR_2: np.ndarray) -> np.ndarray:
+    DATA_1 = DATA_FLOOR_1 - np.mean(DATA_FLOOR_1)
+    DATA_2 = DATA_FLOOR_2 - np.mean(DATA_FLOOR_2)
+
+    DATA_1 = scipy.signal.detrend(DATA_1)
+    DATA_2 = scipy.signal.detrend(DATA_2)
+
+    DATA_1 = DATA_1 - np.mean(DATA_1)
+    DATA_2 = DATA_2 - np.mean(DATA_2)
+
+    cross_correlation = scipy.signal.correlate(DATA_1, DATA_2, mode='full')
+    return cross_correlation
--- Code/PlotFunctions.py
+++ Code/PlotFunctions.py
@@ -1,1 +1,36 @@
+import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import numpy as np
+
+def do_waveform_plot(DATAS: list[np.ndarray], fs: float = 100000) -> str:
+
+    font1 = {'family':'serif', 'color':'black', 'size':20}
+    font2 = {'family':'serif', 'color':'black', 'size':14}
+    plt.ioff()
+
+    filename = f'../Plot/do_waveform_plot.pdf'
+
+    fig = plt.figure(figsize = (16, 12))
+    gs = gridspec.GridSpec(len(DATAS), 1, height_ratios=[1] * len(DATAS))
+
+
+    for i in range(len(DATAS)):
+        WAVEFORM_DATA = DATAS[i]
+
+        TIME = np.linspace(0, len(WAVEFORM_DATA), len(WAVEFORM_DATA))
+        TIME = TIME / fs
+
+        ax = plt.subplot(gs[i, 0])
+        ax.plot(TIME, WAVEFORM_DATA, color = 'C0')
+        ax.set_yticklabels([])
+        ax.set_yticks([])
+        # ax.legend(loc = 'upper right')
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.0, hspace=0.0)
+    plt.savefig(filename, dpi=900, bbox_inches = 'tight')
+    plt.show()
+    plt.close()
+
+    return filename
